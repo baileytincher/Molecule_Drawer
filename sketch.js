77aa28f85e6be49ef8aa0edd3743b9fd19f58ad2
Bailey Tincher <btincher99@gmail.com>
2018-10-29 00:34:04 -0500

    Worked on substituent drawer

--- sketch.js
+++ sketch.js
@@ -6,98 +6,54 @@ var ANG_90 = Math.PI / 2;
 var ANG_60 = ANG_120 / 2;
 var ANG_30 = ANG_60 / 2;
 
-var _molecules = [];
-var linearMolecule
+var molecules_ = [];
+var index = 0;
 
 function setup() {
-  createCanvas(1000, 1000);
-  //pixelDensity(12.0); // Can cause errors on certain displays
+  createCanvas(1000, 500);
+  setupCanvas();
+
+}
+
+function setupCanvas() {
+  clear();
+  stroke(0);
   background(255);
-  //draw();
-  // var linearMolecule = {
-  //   coreCarbons: 5,
-  //   substituents: [
-  //     [],
-  //     [{ formula: "", bondCount: 1, chainLength: 1 }],
-  //     [],
-  //     [],
-  //     [{ formula: "O", bondCount: 2, chainLength: 0 },
-  //       { formula: "OH", bondCount: 1, chainLength: 0 }
-  //     ]
-
-  //   ],
-  //   cyclic: false
-  // };
-  // var linearMolecule = {
-  //   coreCarbons: 4,
-  //   substituents: [
-  //     [{ formula: "OH", bondCount: 1, chainLength: 0 }],
-  //     [{ formula: "Br", bondCount: 1, chainLength: 0 }],
-  //     [{ formula: "Cl", bondCount: 1, chainLength: 0 }],
-  //     [{ formula: "F", bondCount: 1, chainLength: 0 }]
-
-  //   ],
-  //   cyclic: false
-  // };
-  // var linearMolecule = {
-  //   coreCarbons: 3,
-  //   substituents: [
-  //     [],
-  //     [],
-  //     [{ formula: "O", bondCount: 2, chainLength: 0 }]
-
-  //   ],
-  //   cyclic: false
-  // };
-  // linearMolecule = {
-  //   coreCarbons: 4,
-  //   substituents: [
-  //     [{ formula: "OH", bondCount: 1, chainLength: 0 }, { formula: "OH", bondCount: 1, chainLength: 0 }],
-  //     [{ formula: "O", bondCount: 2, chainLength: 0 }],
-  //     [{ formula: "O", bondCount: 2, chainLength: 0 }],
-  //     [{ formula: "OH", bondCount: 1, chainLength: 0 }]
-
-  //   ],
-  //   cyclic: false
-  // };
-  var linearMolecule = {
-    coreCarbons: 3,
-    substituents: [
-      [],
-      [{ formula: "", bondCount: 2, chainLength: 7 }],
-      [{ formula: "", bondCount: 2, chainLength: 6 }]
-
-    ],
-    cyclic: false
-  };
-  loadMolecules('Output.json');
-  drawLinearMolecule(_molecules[0]);
-
-  // var cyclicMolecule = {
-  //   coreCarbons: 3,
-  //   substituents: [
-
-  //     [{ formula: "", bondCount: 1 }],
-  //     [],
-  //     [{ formula: "OH", bondCount: 1 }]
-
-  //   ],
-  //   cyclic: true
-  // };
-  //console.log(linearMolecule);
-  //drawCyclicMolecule(cyclicMolecule);
 
 }
 
-function loadMolecules(file) {
-  var request = new XMLHttpRequest();
-  request.overrideMimeType("application/json");
-  request.open("GET", "Output.json", false);
-  request.send(null)
-  _molecules = JSON.parse(request.responseText);
+/** Accepts a json molecule object and draws it accordingly.
+*
+*   Expects a json object of the form
+    {
+      coreCarbons: unsigned,
+      substituents: [
+        [{ formula: string, bondCount: unsigned, chainLength: unsigned }],
+        ...,
+        ]
+
+      ],
+      cyclic: bool
+    }
+*/
+function loadMolecules(theData) {
+  var molecule = JSON.parse(theData);
+
+  setupCanvas();
+
+  if (molecule.cyclic) {
+    drawCyclicMolecule(molecule);
+
+  } else {
+    drawLinearMolecule(molecule);
+  
+  }
+
 }
 
-// Returns end points of line drawn
+/** Custom function to draw a line on the screen.
+*   returns a point object of the line's end point.
+*/
 function drawLine(angle, length, x0, y0, strokeWeight_ = 2) {
   smooth();
   strokeWeight(strokeWeight_);
@@ -119,6 +75,8 @@ function drawLine(angle, length, x0, y0, strokeWeight_ = 2) {
   return { x: x1, y: y1 };
 }
 
+/** Custom function to add the chemical formula to the screen.
+*/
 function drawText(text_, lineAngle, lineLength, x0, y0, textSize_ = 25) {
   textSize(textSize_);
   strokeWeight(0);
@@ -145,14 +103,12 @@ function drawText(text_, lineAngle, lineLength, x0, y0, textSize_ = 25) {
 
   }
 
-  if (Math.cos(lineAngle) < 0 && text_ == "OH") {
-    text_ = "HO"
-  }
-
   text(text_, x0 + delx, y0 + dely);
 }
 
-// Returns end points of center-line drawn
+/** Draws a double bond on the canvas.
+*   returns the end point of the double bond.
+*/
 function drawDoubleBond(angle, length, x0, y0, strokeWeight_ = 2) {
   // Sines and cosines necessary to stagger second line properly
   var delx = Math.sin(angle) * 6;
@@ -168,34 +124,115 @@ function drawDoubleBond(angle, length, x0, y0, strokeWeight_ = 2) {
 
 }
 
+function drawAlcohol(angle, lineLength, x0, y0) {
+  var subStartPt = drawLine(angle, lineLength, x0, y0);
+
+  var text_ = "OH";
+  if (Math.cos(lineAngle) < 0) {
+    text_ = "HO";
+  }
+
+  drawText(text_, angle, lineLength, x0, y0);
+
+}
+
+function drawHalogens(formulas, coreBonds, angle, lineLength, x0, y0) {
+  var num = formulas.length;
+  var offset;
+  var startAng;
+  if (num == 1) {
+    offset = 0;
+    startAng = angle;
+
+  } else if (num == 2) {
+    if (coreBonds == 0) {
+      offset = 2*ANG_90;
+      startAng = angle;
+
+    } else if (coreBonds == 1) {
+      offset = ANG_120;
+      startAng = angle;
+
+    } else if (coreBonds == 2) {
+      offset = 2/3 * ANG_120;
+      startAng = angle - offset/2;
+
+    }
+
+  } else if (num == 3) {
+    if (coreBonds == 0) {
+      offset = ANG_120;
+      startAng = angle;
+
+    } else if (coreBonds == 1) {
+      offset = 2/3 * ANG_120;
+      startAng = angle - offset/2;
+    }
+
+  } else if (num == 4) {
+    offset = ANG_90;
+    startAng = angle;
+
+  }
+
+  for (var i = 0; i < num; i++) {
+    var textStartPt = drawLine(startAng, lineLength, x0, y0);
+    drawText(formulas[i].formula, startAng, lineLength, textStartPt.x, textStartPt.y);
+    startAng += offset;
+
+  }
+}
+
+function drawCarbonyl(angle, lineLength, x0, y0) {
+  var subStartPt = drawDoubleBond(angle, lineLength, x0, y0);
+  drawText("O", angle, lineLength, x0, y0);
+
+}
+
+function drawSubstituents(substituents, bondCount, angle, lineLength, x0, y0) {
+  switch(substituents[0].type) { //@TODO: FIX THIS TO BE MODULAR FOR MULT TYPES OF SUBS
+    case "CARBONYL": 
+      drawCarbonyl(angle, lineLength, x0, y0);
+
+    case "ALCOHOL":
+      drawAlcohol(angle, lineLength, x0, y0);
+
+    case "HALOGEN":
+      drawHalogens(substituents, bondCount, angle, lineLength, x0, y0);
+
+    case "ALKYL":
+      drawLinearChain(substituents.chainLength, angle, lineLength, x0, y0);
+
+    default:
+      stroke(5, 5, 255); //Denote error
+      drawLinearChain(1, angle, lineLength, x0, y0);
+      stroke(0);
+  }
+
+}
+
+/** Draws an alkyl substituent on the canvas
+*   with the desired chain length.
+*/
 function drawLinearChain(chainLength, angle, lineLength, x0, y0) {
   var startPt = { x: x0, y: y0 };
   var ang = angle;
 
-  stroke(5, 255, 5);
-
   for (var i = 0; i < chainLength; i++) {
     startPt = drawLine(ang, lineLength, startPt.x, startPt.y);
+    ang = (i % 2) == 0 ? ang - ANG_60 : ang + ANG_60;
 
-    if (angle == ANG_90 || angle == -ANG_90 * 3) {
-      ang = (i % 2 == 0) ? ANG_30 : -ANG_30;
-
-    } else if (angle == -ANG_90 || angle == ANG_90 * 3) {
-      ang = (i % 2 == 0) ? -ANG_30 : ANG_30;
-
-    } else if (angle == -ANG_30) {
-      ang = (i % 2 == 0) ? ANG_30 : -ANG_30;
-
-    } else {
-      ang = ((i % 2 == 0) ? -ANG_30 : ANG_30);
-    }
   }
 
   stroke(0);
 
 }
 
-function drawLinearMolecule(molecule, x0 = 200, y0 = 200) {
+/** Draws a "linear" molecule on the screen.
+*   Expects a JSON object of the form described by
+*   loadMolecule().
+*/
+function drawLinearMolecule(molecule, x0 = 150, y0 = 200) {
   var ang = -ANG_30;
   var startPt = { x: x0, y: y0 };
 
@@ -253,6 +290,10 @@ function drawLinearMolecule(molecule, x0 = 200, y0 = 200) {
 
 }
 
+/** Draws a "cyclic" molecule on the screen.
+*   Expects a JSON object of the form described by
+*   loadMolecule().
+*/
 function drawCyclicMolecule(molecule, x0 = 200, y0 = 200) {
   var ang = 0;
   var startPt = { x: x0, y: y0 };
@@ -260,8 +301,91 @@ function drawCyclicMolecule(molecule, x0 = 200, y0 = 200) {
   var del_ang = 2 * Math.PI / molecule.coreCarbons;
 
   for (var i = 0; i < molecule.coreCarbons; i++) {
+    var sub_ang;
+    drawSubstituents(molecule.substituents, 2, sub_ang, lineLength, startPt.x, startPt.y);
     startPt = drawLine(ang, LINE_LENGTH, startPt.x, startPt.y);
     ang += del_ang;
   }
 
 }
+
+
+/** Simple molecule test cases*/
+function drawTestMol() {
+  setupCanvas();
+
+  var mol = {
+    coreCarbons: 4,
+    substituents: [
+      [{ formula: "OH", bondCount: 1, chainLength: 0 }, { formula: "OH", bondCount: 1, chainLength: 0 }],
+      [{ formula: "O", bondCount: 2, chainLength: 0 }],
+      [{ formula: "O", bondCount: 2, chainLength: 0 }],
+      [{ formula: "OH", bondCount: 1, chainLength: 0 }]
+
+    ],
+    cyclic: false
+  };
+
+  drawLinearMolecule(mol);
+
+    stroke(0);
+    line(20, 20, 50, 50);
+
+}
+
+  // var linearMolecule = {
+  //   coreCarbons: 5,
+  //   substituents: [
+  //     [],
+  //     [{ formula: "", bondCount: 1, chainLength: 1 }],
+  //     [],
+  //     [],
+  //     [{ formula: "O", bondCount: 2, chainLength: 0 },
+  //       { formula: "OH", bondCount: 1, chainLength: 0 }
+  //     ]
+
+  //   ],
+  //   cyclic: false
+  // };
+  // var linearMolecule = {
+  //   coreCarbons: 4,
+  //   substituents: [
+  //     [{ formula: "OH", bondCount: 1, chainLength: 0 }],
+  //     [{ formula: "Br", bondCount: 1, chainLength: 0 }],
+  //     [{ formula: "Cl", bondCount: 1, chainLength: 0 }],
+  //     [{ formula: "F", bondCount: 1, chainLength: 0 }]
+
+  //   ],
+  //   cyclic: false
+  // };
+  // var linearMolecule = {
+  //   coreCarbons: 3,
+  //   substituents: [
+  //     [],
+  //     [],
+  //     [{ formula: "O", bondCount: 2, chainLength: 0 }]
+
+  //   ],
+  //   cyclic: false
+  // };
+  // linearMolecule = {
+  //   coreCarbons: 4,
+  //   substituents: [
+  //     [{ formula: "OH", bondCount: 1, chainLength: 0 }, { formula: "OH", bondCount: 1, chainLength: 0 }],
+  //     [{ formula: "O", bondCount: 2, chainLength: 0 }],
+  //     [{ formula: "O", bondCount: 2, chainLength: 0 }],
+  //     [{ formula: "OH", bondCount: 1, chainLength: 0 }]
+
+  //   ],
+  //   cyclic: false
+  // };
+  // var linearMolecule = {
+  //   coreCarbons: 3,
+  //   substituents: [
+  //     [],
+  //     [{ formula: "", bondCount: 2, chainLength: 7 }],
+  //     [{ formula: "", bondCount: 2, chainLength: 6 }]
+
+  //   ],
+  //   cyclic: false
+  // };
